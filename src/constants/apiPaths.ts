246372d9bf6315c908dd35e50bd6cb2f51509a66
HelenBassa <helenbassa@gmail.com>
2023-11-28 21:12:38 +0300

    update apiPath

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
+/* eslint-disable prettier/prettier */
 const API_PATHS = {
   product: "https://d1v4qmrdh4.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://gfk065movg.execute-api.us-east-1.amazonaws.com",
   bff: "https://d1v4qmrdh4.execute-api.us-east-1.amazonaws.com",
-  cart: "https://d1v4qmrdh4.execute-api.us-east-1.amazonaws.com",
+  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
