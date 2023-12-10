ec13676b19366e9f4a5cb82ae8cf1886ea6b3646
HelenBassa <helenbassa@gmail.com>
2023-12-11 00:26:34 +0300

    fix: change import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: "https://d1v4qmrdh4.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://gfk065movg.execute-api.us-east-1.amazonaws.com",
+  import: "https://neietebezi.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://d1v4qmrdh4.execute-api.us-east-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
